11f5da251712b51a01ee40dfdf8a5317a2cd666e
Baron <arthur.baron@epitech.eu>
2019-10-08 09:44:51 +0200

    08/10|09:44|Task 04 partially done. Task 05 strstr deleted because program unfinished.

--- tests/test_my_strncpy.c
+++ tests/test_my_strncpy.c
@@ -7,9 +7,20 @@
 
 #include <criterion/criterion.h>
 
-Test(my_strncpy, copy_char_into_other_string){
-    char dest[6];
+char *my_strncpy(char *dest, char const *src, int n);
+
+Test(my_strncpy, copy_char_into_other_string)
+{
+    char dest[6] = {0};
 
     my_strncpy(dest, "HelloWorld" , 5);
     cr_assert_str_eq(dest, "Hello");
+}
+
+Test(my_strncpy, copie_space_empty_array_to_rename)
+{
+    char dest[12] = {0};
+
+    my_strncpy(dest, "some hello world string right here", 18);
+    cr_assert_str_eq(dest, "hello world");
 }
