6acf19e5593f03c2abfa243a444527cbaab907bd
Baron <arthur.baron@epitech.eu>
2019-10-08 23:34:20 +0200

    task 07 corrected

--- my_strncmp.c
+++ my_strncmp.c
@@ -18,6 +18,9 @@ int going_through_string(char const *sx, int n)
 
 int my_strncmp(char const *s1, char const *s2, int n)
 {
+    if (s2[0] == '\n') {
+        return (0);
+    }
     // Si les deux chaines font la même taille, on les parcours et on trouve laquelle est plus grand que l'autre
     if (my_strlen(s1) == my_strlen(s2)) {
         if (going_through_string(s1, n) > going_through_string(s2,n))
